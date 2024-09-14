3b485b10ff31882b2a880960401a7e150148c400
DarkWister <darkwistercrak17@gmail.com>
2024-09-14 18:11:37 -0400

    Update repository data

---
+++ Ejercicios_v2/ex0/Program.cs
@@ -0,0 +1,21 @@
+﻿//PROGRAMA QUE LEA UN NUMERO Y DETERMINE SI ES POSITIVO O NEGATIVO, MOSTRANDO UN MENSAJE PARA CADA CASO.
+using System;
+namespace hell
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine()!);
+            if (num > 0){
+                Console.WriteLine("Este numero es positivo!");
+            }
+            else if(num == 0){
+                Console.WriteLine("Ingresaste cero");
+            }
+            else{
+                Console.WriteLine("")
+            }
+        }
+    }    
+}  
